4cfed9d36527ef3b225c9394852b4e737f9b9e18
sitkom <contact@sit-kom.com>
2015-09-02 23:04:17 +0200

    First commit

---
+++ app/src/androidTest/java/cafec/com/myapplication/DisplayMessageActivityTest.java
@@ -0,0 +1,10 @@
+package cafec.com.myapplication;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by gilles on 02/09/2015.
+ */
+public class DisplayMessageActivityTest extends TestCase {
+
+}
